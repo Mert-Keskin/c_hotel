937a83339f00776e1d902850257037dd9bd68913
Mert-Keskin <prokaryote_@hotmail.com>
2021-12-20 00:29:28 +0300

    empt_room eklendi ve show_info duzeltildi

--- c_hotel.c
+++ c_hotel.c
@@ -2,6 +2,7 @@
 #include <string.h>
 void book(int** rooms,char names[6][25]);
 void show_infos(int** rooms,char names[6][25]);
+void empty_room(int** rooms,char names[6][25]);
 int main()
 {
     int room1[3]={1,1,1};int room2[3]={1,2,0};int room3[3]={2,3,0};int room4[3]={2,4,0};int room5[3]={3,5,0};int room6[3]={3,6,0};
@@ -9,12 +10,14 @@ int main()
     int* rooms[6]={room1,room2,room3,room4,room5,room6};
     char islem;
     do{
-        printf("Cikmak icin q\nBilgileri görmek icin b\nRezervasyon icin r\nOda bosaltmak icin s : ");
+        printf("-----------------\nCikmak icin q\nBilgileri görmek icin b\nRezervasyon icin r\nOda bosaltmak icin s : ");
         scanf(" %c",&islem);
         if(islem =='b')
             show_infos(rooms,names);
         else if (islem == 'r')
             book(rooms,names);
+        else if (islem == 's')
+            empty_room(rooms,names);
     }
     while(islem !='q');
     return 0;
@@ -56,12 +59,20 @@ void show_infos(int** rooms,char names[6][25]){
     printf("1 kisilik bos oda sayisi : %d\n2 kisilik bos oda sayisi : %d\n3 kisilik bos oda sayisi : %d\n",arr[0],arr[1],arr[2]);
     int n;
     char n2;
-    do{
+    printf("Devam edip ayrintili bilgi görmek icin d cickmak icin q : ");
+    scanf(" %c",&n2);
+    while((n2!='q')&&(n2!='Q')){
         printf("Ayrintili bilgi icin oda numarasi giriniz : ");
         scanf(" %d",&n);
         printf("Oda sahibinin adi %s\nOdadaki kisi sayisi %d\n",names[n-1],rooms[n-1][0]);
         printf("Devam icin d cickmak icin q : ");
         scanf(" %c",&n2);
     }
-    while((n2!='q')&&(n2!='Q'));
+}
+void empty_room(int** rooms,char names[6][25]){
+    int i;
+    printf("Bosaltmak isediginiz odayi giriniz");
+    scanf(" %d",&i);
+    rooms[i-1][2]=0;
+    strcpy(names[i-1],"");
 }
